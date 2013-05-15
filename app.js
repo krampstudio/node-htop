61bc135e9d43d07689a85e8f4a527b7f23dc4f3a
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-05-15 07:55:46 -0400

    add node-mem poller and update statpoller test

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.errorHandler());
 
 //controller.index(app);
 
+console.log('test')
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- test/statpoller_test.js
+++ test/statpoller_test.js
@@ -1,3 +1,5 @@
+var _ = require('underscore');
+
 /**
  * @fileoverview NodeUnit test for the StatPoller class
  * @author Bertrand Chevrier <chevrier.bertrand@gmail.com>
@@ -38,22 +40,75 @@ exports.StatPollerTest = {
         
         var otherPoller = new this.StatPoller({delay : 775});
         test.equal(otherPoller.options.delay, 775, 'the value of the delay should be 775ms');
-
+        
         test.done();
     },
     
     'testStart': function(test) {
         'use strict';
         
-        test.expect(1);
+        test.expect(2);
         
         var aPoller = new this.StatPoller();
         aPoller.on('start', function(){
             test.ok(true, 'the poller has started');
+            test.equals(_.keys(aPoller.pollers).length, aPoller.intervalHandlers.length, 'there should be the same number of handlers than pollers')
             test.done();
         });
         aPoller.start();
         aPoller.stop();
+    },
+    
+   'testStop': function(test) {
+        'use strict';
+        
+        test.expect(2);
+        
+        var aPoller = new this.StatPoller();
+        aPoller.on('stop', function(){
+            test.ok(true, 'the poller has stopped');
+            test.equals(aPoller.intervalHandlers.length, 0, 'all handlers references should have been removed');
+            test.done();
+        });
+        aPoller.start();
+        aPoller.stop();
+    },
+   
+    'testFilterPollers': function(test) {
+        'use strict';
+        
+        test.expect(2);
+        
+        var aPoller = new this.StatPoller({pollers : ['mem']});
+        
+        aPoller.on('start', function(){
+            test.ok(true, 'the poller has started');
+            test.equals(_.keys(aPoller.pollers).length, 1, 'there should be only one handler');
+            test.done();
+        });
+        aPoller.start();
+        aPoller.stop();
+    },
+    
+    'testPoll': function(test) {
+        'use strict';
+    
+        test.expect(6);
+    
+        var aPoller = new this.StatPoller({
+            pollers: ['node-mem']
+        }),
+            counter = 1;
+    
+        aPoller.on('stat', function(data) {
+            test.equals(data.type, 'node-mem');
+            test.ok(typeof data.values === 'object');
+            if (++counter > 3) {
+                aPoller.stop();
+                test.done();
+            }
+        });
+        aPoller.start();
     }
 
 };
