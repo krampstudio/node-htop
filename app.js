b29d1e43e0c06ba462efee2766a4b69bd7407cd8
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2013-05-20 21:51:40 +0200

    add sysinfos

--- app.js
+++ app.js
@@ -7,24 +7,23 @@ var express = require('express'),
     io = require('socket.io').listen(server),
     logger = require('./lib/logger')(7);
 
-
+//set up middleware
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-app.use(express.cookieParser('xxx'));
+app.use(express.cookieParser('secret'));
 app.use(express.session());
 app.use(app.router);
-/*app.use(require('less-middleware')({
-	src: __dirname + '/public/css',
-	debug : true
-}));*/
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.errorHandler());
 
+//set up controlllers
+controllers.sysinfos(app);
 controllers.stat(io);
 
+//start the server
 server.listen(app.get('port'), function listenServer(){
   logger.log('info', 'Express server listening on port ' + app.get('port'));
 });
--- controller/sysinfos.js
+++ controller/sysinfos.js
@@ -1,7 +1,10 @@
+var sysinfos = require('../lib/sysinfos');
+
 module.exports = function(app){
     app.get('/sysinfos', function(req, res){
         res.json({
-            'ok': true
+            'platform': sysinfos.getPlatform(),
+            'cpus' : sysinfos.getCpus()
         });
     });
 };
