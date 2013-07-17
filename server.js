3a8e47507c2a3335903e9eabe2c7fb58f5eb039e
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-07-17 07:34:54 -0400

    update doc

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var cluster = require('cluster');
 var cpus = require('os').cpus().length;
 
-console.log(cpus + ' CPUs found');
-
 if(cluster.isMaster){
     for (var i = 0; i < cpus; i++){
         cluster.fork();
